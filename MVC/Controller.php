eb1c2eed5bfa58a1f9e2149ed21ab32145e3b9c5
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-17 23:35:13 +0700

    Return for response for Kecik Version 1.0.4

--- MVC/Controller.php
+++ MVC/Controller.php
@@ -44,8 +44,16 @@ class Controller {
 	 * @param string $file
 	 * @param array $param
 	 **/
-	protected function view($file, $param=array()) {
+	protected function view($file, $param=[]) {
+		ob_start();
 		extract($param);
-		include Config::get('path.mvc').'/views/'.$file.'.php';
+		$myfile = fopen(Config::get('path.mvc').'/views/'.$file.'.php', "r");
+		$view = fread($myfile,filesize(Config::get('path.mvc').'/views/'.$file.'.php'));
+		fclose($myfile);
+		//$view = file_get_contents( Config::get('path.mvc').'/views/'.$file.'.php' );
+		eval('?>'.$view);
+		$result = ob_get_clean();
+		
+		return $result;
 	}
 }
